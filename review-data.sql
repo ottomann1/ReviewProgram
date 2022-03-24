de80b287e0f7ac136c92230be4925e9703453780
Joan-barcelona <32038566+Joan-barcelona@users.noreply.github.com>
2022-03-24 09:56:56 +0100

    Add files via upload

--- review-data.sql
+++ review-data.sql
@@ -1,32 +1,32 @@
 
 
 
-INSERT INTO customer(customer_id, first_name, last_name, phone_number, email_address) VALUES (1, 'John', 'Doe', '345876123', 'johndoe@gmail.com');
-INSERT INTO customer(customer_id, first_name, last_name, phone_number, email_address) VALUES (2, 'Jane', 'Doe', '569301347', 'janedoe@gmail.com');   
-INSERT INTO customer(customer_id, first_name, last_name, phone_number, email_address) VALUES (3, 'William', 'Smith', '603485109', 'willsmith@gmail.com');
-INSERT INTO customer(customer_id, first_name, last_name, phone_number, email_address) VALUES (4, 'Gabriel', 'Simpson', '296024759', 'gabsimpson@gmail.com');
-INSERT INTO customer(customer_id, first_name, last_name, phone_number, email_address) VALUES (5, 'Edward', 'Tyson', '904532861', 'edtyson@gmail.com');
+INSERT INTO customer(first_name, last_name, phone_number, email_address) VALUES ( 'John', 'Doe', '345876123', 'johndoe@gmail.com');
+INSERT INTO customer(first_name, last_name, phone_number, email_address) VALUES ( 'Jane', 'Doe', '569301347', 'janedoe@gmail.com');   
+INSERT INTO customer(first_name, last_name, phone_number, email_address) VALUES ( 'William', 'Smith', '603485109', 'willsmith@gmail.com');
+INSERT INTO customer(first_name, last_name, phone_number, email_address) VALUES ( 'Gabriel', 'Simpson', '296024759', 'gabsimpson@gmail.com');
+INSERT INTO customer(first_name, last_name, phone_number, email_address) VALUES ( 'Edward', 'Tyson', '904532861', 'edtyson@gmail.com');
 
 
-INSERT INTO address(address_id, street_name, street_number, postal_number, city) VALUES (1, 'Kungsgatan', 56, '73490', 'Stockholm');
-INSERT INTO address(address_id, street_name, street_number, postal_number, city) VALUES (2, 'Drottningsgatan', 23, '86342', 'Goteborg');
-INSERT INTO address(address_id, street_name, street_number, postal_number, city) VALUES (3, 'Skolgatan', 98, '91265', 'Malmo');
-INSERT INTO address(address_id, street_name, street_number, postal_number, city) VALUES (4, 'Kyrkogatan', 4, '75692', 'Uppsala');
-INSERT INTO address(address_id, street_name, street_number, postal_number, city) VALUES (5, 'Marknadsgatan', 29, '16438', 'Umea');
+INSERT INTO address(street_name, street_number, postal_number, city) VALUES ('Kungsgatan', 56, '73490', 'Stockholm');
+INSERT INTO address(street_name, street_number, postal_number, city) VALUES ('Drottningsgatan', 23, '86342', 'Goteborg');
+INSERT INTO address(street_name, street_number, postal_number, city) VALUES ('Skolgatan', 98, '91265', 'Malmo');
+INSERT INTO address(street_name, street_number, postal_number, city) VALUES ('Kyrkogatan', 4, '75692', 'Uppsala');
+INSERT INTO address(street_name, street_number, postal_number, city) VALUES ('Marknadsgatan', 29, '16438', 'Umea');
 
 
-INSERT INTO restaurant(restaurant_id, restaurantName, address_id, restaurantDescription) VALUES (1, 'McDonalds', 3, 'Hamburger restaurant.');
-INSERT INTO restaurant(restaurant_id, restaurantName, address_id, restaurantDescription) VALUES (2, 'Pizza Hut', 5, 'Pizzeria'); 
-INSERT INTO restaurant(restaurant_id, restaurantName, address_id, restaurantDescription) VALUES (3, 'Sushi Kyoto', 1, 'Japanesse restaurant.');
-INSERT INTO restaurant(restaurant_id, restaurantName, address_id, restaurantDescription) VALUES (4, 'Kebab Bagdad', 2, 'Kebab restaurant.');
-INSERT INTO restaurant(restaurant_id, restaurantName, address_id, restaurantDescription) VALUES (5, 'Kentucky Fried Chicken', 4, 'Fried chicken restaurant.');
+INSERT INTO restaurant(restaurantName, address_id, restaurantDescription) VALUES ('McDonalds', 3, 'Hamburger restaurant.');
+INSERT INTO restaurant(restaurantName, address_id, restaurantDescription) VALUES ('Pizza Hut', 5, 'Pizzeria'); 
+INSERT INTO restaurant(restaurantName, address_id, restaurantDescription) VALUES ('Sushi Kyoto', 1, 'Japanesse restaurant.');
+INSERT INTO restaurant(restaurantName, address_id, restaurantDescription) VALUES ('Kebab Bagdad', 2, 'Kebab restaurant.');
+INSERT INTO restaurant(restaurantName, address_id, restaurantDescription) VALUES ('Kentucky Fried Chicken', 4, 'Fried chicken restaurant.');
 
 
-INSERT INTO review(review_id, customer_id, restaurant_id, stars, reviewText) VALUES (1, 3, 4, 4, 'My favourite restaunrant.');
-INSERT INTO review(review_id, customer_id, restaurant_id, stars, reviewText) VALUES (2, 1, 2, 3, 'Food is very tasty.');
-INSERT INTO review(review_id, customer_id, restaurant_id, stars, reviewText) VALUES (3, 4, 5, 5, 'I like to eat there every day.');
-INSERT INTO review(review_id, customer_id, restaurant_id, stars, reviewText) VALUES (4, 2, 3, 1, 'Very expensive and not very good food.');
-INSERT INTO review(review_id, customer_id, restaurant_id, stars, reviewText) VALUES (5, 5, 1, 3, 'You get good food for a good price.');
+INSERT INTO review(customer_id, restaurant_id, stars, reviewText) VALUES (3, 4, 4, 'My favourite restaunrant.');
+INSERT INTO review(customer_id, restaurant_id, stars, reviewText) VALUES (1, 2, 3, 'Food is very tasty.');
+INSERT INTO review(customer_id, restaurant_id, stars, reviewText) VALUES (4, 5, 5, 'I like to eat there every day.');
+INSERT INTO review(customer_id, restaurant_id, stars, reviewText) VALUES (2, 3, 1, 'Very expensive and not very good food.');
+INSERT INTO review(customer_id, restaurant_id, stars, reviewText) VALUES (5, 1, 3, 'You get good food for a good price.');
 
 
 
--- review-schema.sql
+++ review-schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE review;
 USE review;
 
 CREATE TABLE customer(
-customer_id int NOT NULL PRIMARY KEY,
+customer_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name varchar(50) NOT NULL,
 last_name varchar(50) NOT NULL,
 phone_number varchar(30),
@@ -13,7 +13,7 @@ email_address varchar(50)
 );
 
 CREATE TABLE address(
-address_id int NOT NULL PRIMARY KEY,
+address_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 street_name varchar(50),
 street_number int,
 postal_number char(5),
@@ -21,7 +21,7 @@ city varchar(50)
 );
 
 CREATE TABLE restaurant(
-restaurant_id int NOT NULL PRIMARY KEY,
+restaurant_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 restaurantName varchar(50),
 address_id int,
 restaurantDescription varchar(255),
@@ -29,7 +29,7 @@ FOREIGN KEY(address_id) REFERENCES address(address_id)
 );
 
 CREATE TABLE review(
-review_id int NOT NULL PRIMARY KEY,
+review_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 customer_id int NOT NULL,
 restaurant_id int NOT NULL,
 stars int,
